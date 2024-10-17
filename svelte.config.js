655520314bf394373eb729d33a2ef74bb1091d2b
Dimitrij Samus <d.samus@globit.com>
2024-10-17 21:52:17 +0200

    Add svelte-logo.svg

--- svelte.config.js
+++ svelte.config.js
@@ -3,5 +3,8 @@ import adapter from '@sveltejs/adapter-vercel'
 export default {
   kit: {
     adapter: adapter(),
+    prerender: {
+      handleMissingId: 'warn' // Это позволит игнорировать ошибки 404 при пререндеринге
+    }
   }
 }
